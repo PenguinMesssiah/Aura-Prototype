39d7dea04868bedfce599e2be35ed080d8bf6b9a
Will Scott <wscott2@andrew.cmu.edu>
2025-07-08 14:09:59 -0400

    Iteration #3 [UI Done] [Backend Need]

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 //Initialize Konva, Size, & Colors
 const canvasWidth         = 1200
-const canvasHeight        = 700
+const canvasHeight        = 725
 const canvasWidthMargins  = 1100
 const canvasHeightMargins = 600
 const deltaWidth          = (canvasWidth-canvasWidthMargins)/2
@@ -15,18 +15,18 @@ const stage = new Konva.Stage({
     height: canvasHeight,
     draggable: false
 });
-const nodeLayer       = new Konva.Layer();
-const textLayer       = new Konva.Layer();
+var nodeLayer       = new Konva.Layer();
+var textLayer       = new Konva.Layer();
 const nodeFillColor   = '#8AB3D7' 
 const nodeStrokeColor = '#2170B4'
 const period          = 2000
 const delta           = 50
 
-const webView = document.getElementById('web-view')
-var nodeList = []
-var textList = []
+const webView  = document.getElementById('web-view')
+var progression = 0;
+var stageJson;
 var animList = []
-var cardList = []
+var finalConsiderations = []
 
 
 //Linking
@@ -35,7 +35,7 @@ function init() {
 }
 
 function linkEvents() {
-    //Link Stage to Layer
+    //Link Stage to Layers
     stage.add(nodeLayer);
     stage.add(textLayer);
 
@@ -52,9 +52,15 @@ function linkEvents() {
         //Check for Start Animation & Hide Processing Msg
         if(!initalCall && animList[0]?.isRunning()) {
             animList[0].stop()
-            textList[0]?.hide()
+
+            let processingMsg = textLayer.find('.Processing_Message')[0]
+            processingMsg.hide();
 
             drawSubNodes();
+            drawSubNodeTextSet();
+
+            //Save Here
+            stageJson = stage.toJSON();
         }
         
         //Log Initial Message
@@ -74,24 +80,10 @@ function linkEvents() {
         }
     })
     
-    var matchingCard;
+    /*
     stage.on('click', function (e) {
-        //Error Handling       
-        if(typeof e.target.id() != 'number') {
-            console.log("Canvas Error Handler: Clicked on Invalid Canvas Location")
-            return
-        }
-
-        matchingCard = cardList[e.target.id()]
-        //console.log('matchingCard', matchingCard)
-        //console.log('cardList = ', cardList)
-        
-        //Toggle Visibility
-        if(matchingCard.style.visibility == 'hidden') 
-            matchingCard.style.visibility = 'visible';
-        else 
-            matchingCard.style.visibility = 'hidden';
     })
+    */
 }
 
 //Drawing Functions 
@@ -156,8 +148,9 @@ function drawStartAnimation() {
     //Scaling Animation
     var anim = new Konva.Animation(function(frame) {
         const scale = Math.sin(frame.time * 2 * Math.PI / period) + 5.5;
-
-        nodeList[0].scale({ x: scale/4.75, y: scale/4.75 });
+        
+        let centralNode = nodeLayer.find('.Central_Node')[0]
+        centralNode.scale({ x: scale/4.75, y: scale/4.75 });
     }, nodeLayer);
 
     animList.push(anim)
@@ -170,7 +163,7 @@ function drawCentralNode() {
         y: canvasHeightMargins/1.75, //343
         radius: 175,
         name: "Central_Node",
-        id: nodeList.length,
+        id: 0,
         fillLinearGradientStartPoint: { x: -50, y: -50 },
         fillLinearGradientEndPoint: { x: 50, y: 50 },
         fillLinearGradientColorStops: [0, '#CE3608', 1, '#FFA931'],
@@ -178,63 +171,56 @@ function drawCentralNode() {
         zindex: 2
     })
 
-    nodeList.push(circle)
     nodeLayer.add(circle)
 }
 
 function next() {
     animList[0].stop()
-    textList[0]?.hide()
+    
+    let processingMsg = textLayer.find('.Processing_Message')[0]
+    processingMsg.hide();
 
     drawSubNodes()
-    drawDetails()
+    drawSubNodeTextSet()
+
+    stageJson = {
+        nodeLayer: nodeLayer.toJSON(),
+        textLayer: textLayer.toJSON()
+    }
+    //console.log("Stage Json = ", stageJson )
 }
 
-function drawExplorationNode() {
-    //Hide Other Txt Elements
-    textList.forEach((element => {
-        if(element.id() != 1) element.hide()
+function drawExplorationNode(subNode_idx) {
+    let centerNode          = nodeLayer.find('.Central_Node')[0]
+    let centerSubNode       = nodeLayer.find('.Sub_Node_1')[0]
+    let centerSubNodeHeader = textLayer.find('.subNodeHeader_1')[0]
+    let centerSubNodeBody   = textLayer.find('.subNodeContent_1')[0]
+    let textNodes           = textLayer.find('Text')
+    let subNodes            = nodeLayer.find('Circle')
+    let btns                = nodeLayer.find('Label')
+    
+    //Hide Elements
+    textNodes.forEach((element => {
+        if(element.name() != 'subNodeHeader_1' &&
+            element.name() != 'subNodeContent_1') element.hide()
     }));
-    //Hide Other Node Elements
-    nodeList.forEach((element => {
-        if(element.id() != 2 //Sub Node 2
-            && element.id() != 0 //Center Node
-            && element.id() != 5) //Explore Btn 
-            element.hide()
+    subNodes.forEach((element => {
+        if(element.name() != 'Sub_Node_1' && element.name() != 'Central_Node') element.hide()
     }))
-
-    //Perform Expansion: If time, add animation here
-    /*
-    let amplitude = 50;
-    const anim = new Konva.Animation(function(frame) {
-    nodeList[2].x(
-        amplitude * Math.sin((frame.time * 2 * Math.PI) / period) +
-        window.innerWidth / 2
-        );
-    }, nodeLayer);
-    anim.start();
-    */
-    let centerNode = nodeLayer.find('.Central_Node')[0]
-    let centerSubNode    = nodeLayer.find('.Sub_Node_1')[0]
-    let centerSubNodeTxt = textLayer.find('.Sub_Node_1_Txt')[0]
-    let centerSubNodeBtn = nodeLayer.find('.Sub_Node_Btn')[1]
-
-    //console.log("centerSubNode = ", centerSubNode)
-    //console.log("centerSubNodeTxt = ", centerSubNodeTxt)
-    //console.log("centerSubNodeBtn = ", centerSubNodeBtn)
+    btns.forEach((element => {
+        element.hide()
+    }))
 
     //Reposition Canvas Elements
     centerNode.x(canvasWidthMargins/2-500)
     centerNode.radius(175)
-    centerSubNode.x(subNodePos[1].x+35) //Update Sub Node 2
-    centerSubNodeTxt.x(subNodePos[1].x+100) //Update Sub Node 2 Text
-    centerSubNodeBtn.x(subNodePos[1].x+100) //Update Sub Node 2 Btn
+    centerSubNode.x(subNodePos[1].x+35)
+    centerSubNodeHeader.x(subNodePos[1].x+100)
+    centerSubNodeBody.x(subNodePos[1].x+100)
 
-    //console.log("centerNode.x = ", centerNode.x())
-    //console.log("centerSubNode.x = ", centerSubNode.x())
-    //console.log("centerNode.getAbsoluteScale().x = ", centerNode.getAbsoluteScale().x)
+    //Update SubNode Text To Match from (subNode_idx)
 
-    // line segments with a length of 33px with a gap of 10px
+
     let startPt = centerNode.x()+centerNode.radius()*centerNode.getAbsoluteScale().x
     var baseLine = new Konva.Line({
         points: [startPt, centerSubNode.y(), centerSubNode.x()-centerSubNode.radius(), centerSubNode.y()],
@@ -242,13 +228,90 @@ function drawExplorationNode() {
         strokeWidth: 2,
         lineJoin: 'round'
     });
-
     nodeLayer.add(baseLine)
     
-    //Draw Entire Content Node
-    let idx = 0;
+    //Draw Content Nodes
     drawContentNode(startPt,150,centerSubNode.y(),0)
     drawContentNode(startPt,425,centerSubNode.y(),1)
+
+    //Draw & Link Btns
+    drawButton(subNodePos[1].x+100, centerSubNode.y()-20,'Add to System',3) 
+    drawButton(stage.width()-150, stage.height()-100,'Back',4)
+    if(!progression) {
+        drawButton(stage.width()-110, stage.height()-100,'Explore Alternatives',5)
+        
+        let altBtn     = nodeLayer.find(".Sub_Node_Btn_5")[0]
+        altBtn.on('click', () => {
+            progression = 1;
+            redrawDecisionSpace()
+        })
+    }
+    else {
+        drawButton(stage.width()-110, stage.height()-100,'Finish',6)
+        
+        let finishBtn  = nodeLayer.find(".Sub_Node_Btn_6")[0]
+        finishBtn.on('click', () => {
+            drawActionPoints()
+        })
+    }
+
+    let subNodeBtn = nodeLayer.find(".Sub_Node_Btn_3")[0]
+    let backBtn    = nodeLayer.find(".Sub_Node_Btn_4")[0]
+    
+    subNodeBtn.on('click', () => {
+        let consideration = {
+            title: centerSubNodeHeader.getText(),
+            content: centerSubNodeBody.getText(),
+        }
+        finalConsiderations.push(consideration)
+        
+        console.log("Renderer | Consideration Added to System", consideration)
+    })
+    backBtn.on('click', () => {
+        redrawDecisionSpace()
+    })
+}
+
+function drawActionPoints() {
+    stage.destroyChildren()
+
+    //Draw/show action points
+    let actionGroup = document.getElementsByClassName('card-group')
+    console.log("actionGroup = ", actionGroup)
+    console.log("actionGroup = ", actionGroup.item(0))
+    //document.getElementById('actionPointGroup')
+    
+    actionGroup.item(0).style.visibility = 'visible'
+}
+
+function redrawDecisionSpace(pIdx) {
+    //Load Old State of Canvas
+    stage.destroyChildren();
+    nodeLayer = Konva.Node.create(stageJson.nodeLayer)
+    textLayer = Konva.Node.create(stageJson.textLayer)
+    stage.add(nodeLayer)
+    stage.add(textLayer)
+    //Link Explore Btns
+    for(let i=0;i<3;i++) {
+        let subNodeBtn = nodeLayer.find(".Sub_Node_Btn_" + i.toString())[0]
+        //console.log(subNodeBtn)
+        subNodeBtn.on('click', () => {
+            drawExplorationNode(i)
+        })
+    }
+
+    //Change Color
+    if(progression) {
+        for(let i=0;i<3;i++) {
+            let centralNode = nodeLayer.find('.Central_Node')[0]
+            let subNode   = nodeLayer.find('.Sub_Node_'+i.toString())[0]
+
+            //[0, '#CE3608', 1, '#FFA931']
+            centralNode.fillLinearGradientColorStops([0, '#CB98C9', 1, '#9600BC'])
+            subNode.fill('#9600BC')
+            subNode.stroke('#9600BC')
+        }
+    }
 }
 
 function drawContentNode(pStartX,xIndex,pStartY, idx) {
@@ -258,8 +321,8 @@ function drawContentNode(pStartX,xIndex,pStartY, idx) {
     switch(idx) {
         case 0:
             y = pStartY-125;
-            stakeholder_x = x-delta
-            stakeholder_y = pStartY+20
+            stakeholder_x = x-width/2
+            stakeholder_y = pStartY-160
             content_x     = x-delta-75
             content_y     = pStartY+60
             header_x      = x-width/2
@@ -267,10 +330,10 @@ function drawContentNode(pStartX,xIndex,pStartY, idx) {
             break;
         case 1:
             y = pStartY+125;
-            stakeholder_x = x-delta
-            stakeholder_y = pStartY-20
+            stakeholder_x = x-width/2
+            stakeholder_y = pStartY+140
             content_x     = x-delta-75
-            content_y     = pStartY-80
+            content_y     = pStartY-90
             header_x      = x-width/2
             header_y      = pStartY-125
             break;
@@ -279,7 +342,6 @@ function drawContentNode(pStartX,xIndex,pStartY, idx) {
     let circle = new Konva.Circle({
         radius: 12,
         name: "Content_Dot",
-        id: nodeList.length,
         x: x,
         y: pStartY,
         fill: 'black',
@@ -319,68 +381,67 @@ function drawContentNode(pStartX,xIndex,pStartY, idx) {
         fontFamily: 'Poppins',
         fill: 'black'
     });
+    //Draw Stakeholder Title
+    let stakeholderTxt = new Konva.Text({
+        x: stakeholder_x,
+        y: stakeholder_y,
+        width: width, 
+        height: height,
+        name: 'Content_Node_Stakeholder',
+        text: 'Employee',
+        align: 'center',
+        fontSize: 18,
+        fontFamily: 'Poppins',
+        fill: 'black'
+    });
 
-    textList.push(contentHeader)
-    textList.push(contentBody)
-    nodeList.push(circle)
     nodeLayer.add(circle)
     nodeLayer.add(vertLine)
     textLayer.add(contentHeader)
     textLayer.add(contentBody)
+    textLayer.add(stakeholderTxt)
 }
 
-function drawDetails() {
-   //Sub Node 0: Details & Button
-    var subNodeText_0 = new Konva.Text({
-        x: subNodePos[0].x+70,
-        y: subNodePos[0].y-40,
-        id: 0,
-        text: 'Unintended Consequence\nbody goes here',
-        fontSize: 16,
-        fontFamily: 'Calibri',
-        fill: 'black'
-    });
-    drawButton(subNodePos[0].x+70, subNodePos[0].y-40)
+function drawSubNodeTextSet() {
+    for(let i=0;i<3;i++) {
+        var subNodeHeader = new Konva.Text({
+            x: subNodePos[i].x+70,
+            y: subNodePos[i].y-40,
+            name: 'subNodeHeader_' + i.toString(),
+            text: 'Unintended Consequence',
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            fill: 'black'
+        });
 
-    var subNodeText_1 = new Konva.Text({
-        x: subNodePos[1].x+70,
-        y: subNodePos[1].y-40,
-        name:'Sub_Node_1_Txt',
-        id: 1,
-        text: 'Unintended Consequence\nbody goes here',
-        fontSize: 16,
-        fontFamily: 'Calibri',
-        fill: 'black'
-    });
-    drawButton(subNodePos[1].x+70, subNodePos[1].y-40)
+        var subNodeContent = new Konva.Text({
+            x: subNodePos[i].x+70,
+            y: subNodePos[i].y-10,
+            name: 'subNodeContent_' + i.toString(),
+            text: 'Unintended Consequence Body Content',
+            fontSize: 12,
+            fontFamily: 'Calibri',
+            fill: 'black'
+        });
 
-    var subNodeText_2 = new Konva.Text({
-        x: subNodePos[2].x+70,
-        y: subNodePos[2].y-40,
-        id: 2,
-        text: 'Unintended Consequence\nbody goes here',
-        fontSize: 16,
-        fontFamily: 'Calibri',
-        fill: 'black'
-    });
-    drawButton(subNodePos[2].x+70, subNodePos[2].y-40)
+        drawButton(subNodePos[i].x+70, subNodePos[i].y-20, 'Explore', i)
 
-    //Update List
-    textList.push(subNodeText_0)
-    textList.push(subNodeText_1)
-    textList.push(subNodeText_2)
+        //Link Explore Btns
+        let subNodeBtn = nodeLayer.find(".Sub_Node_Btn_" + i.toString())[0]
+        subNodeBtn.on('click', () => {
+            drawExplorationNode(i)
+        })
 
-    textLayer.add(subNodeText_0)
-    textLayer.add(subNodeText_1)
-    textLayer.add(subNodeText_2)
+        textLayer.add(subNodeHeader)
+        textLayer.add(subNodeContent)
+    }
 }
 
-function drawButton(pX, pY) {
+function drawButton(pX, pY, label, i) {
     var button = new Konva.Label({
         x: pX,
         y: pY+50,
-        name: 'Sub_Node_Btn',
-        id: nodeList.length,
+        name: 'Sub_Node_Btn_' + i.toString(),
         opacity: 0.75
     });
 
@@ -395,15 +456,13 @@ function drawButton(pX, pY) {
     }));
 
     button.add(new Konva.Text({
-        text: 'Explore',
+        text: label,
         fontFamily: 'Calibri',
         fontSize: 12,
         padding: 5,
         fill: 'black'
     }));
-    //Update List
-    nodeList.push(button)
-
+    
     nodeLayer.add(button)
 }
 
@@ -411,14 +470,12 @@ function drawProcessingMessage() {
     let processingMsg = new Konva.Text({
         x: canvasWidthMargins/3.25,
         y: 35,
-        id: 99,
+        name: 'Processing_Message',
         text: 'Processing your ethical dilemma and\nsystem data to tailor the best response...',
         fontSize: 30,
         fontFamily: 'Poppins',
         fill: 'black'
     });
-
-    textList.push(processingMsg)
     textLayer.add(processingMsg)
 }
 
@@ -426,8 +483,7 @@ function drawSubNodes() {
     for(let i=0; i<subNodePos.length; i++) {
         let circle = new Konva.Circle({
             radius: 50,
-            name: "Sub_Node_" + (i).toString(),
-            id: nodeList.length,
+            name: "Sub_Node_" + i.toString(),
             x: subNodePos[i].x,
             y: subNodePos[i].y,
             fill: '#FF8650',
@@ -435,12 +491,8 @@ function drawSubNodes() {
             strokeWidth: 1,
             zindex: 2
         })
-
-        nodeList.push(circle)
         nodeLayer.add(circle)
     }
-
-    //console.log('nodeList = ', nodeList.at(0))
 }
 
 function addLLM_Response(pPrompt) {
@@ -488,13 +540,10 @@ function callAPI() {
     window.LLM.sendMsg(userPromptField.value);
 
     //Draw Opening Node & Text
-    if(nodeList.length == 0) {
+    let nodeCheck = nodeLayer.find('.Central_Node')
+    if(nodeCheck.length == 0) {
         drawStartAnimation()
     }
-}
-
-function getRandomNumber(pMinIndex, pMaxIndex) {
-    return Math.floor(Math.random() * (pMaxIndex - pMinIndex) + pMinIndex)
 }
 
 
